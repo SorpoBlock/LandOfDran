383666de4863c1c533bc43dbc1fa77ef5a8128e1
SorpoBlock <syerjchep@gmail.com>
2024-06-07 12:54:42 -0400

    Update RenderContext.cpp

--- Graphics/RenderContext.cpp
+++ Graphics/RenderContext.cpp
@@ -43,10 +43,68 @@ void RenderContext::setSize(unsigned int x, unsigned int y)
 RenderContext::RenderContext(SettingManager & settings) 
 	: context((SDL_GLContext)nullptr) // Exists to suppress warning, SDL_GL_CreateContext can return 0 anyway
 {
-	
+	scope("RenderContext::RenderContext");
+
+	debug("Creating window");
+
+	width = settings.getInt("graphics/startresolutionx");
+	if(width < 0 || width > 10000)
+	{
+		error("graphics/startresolutionx invalid value " + std::to_string(width);
+		return;
+	}
+
+	height = settings.getInt("graphics/startresolutiony");
+	if(height < 0 || height > 10000)
+	{
+		error("graphics/startresolutiony invalid value " + std::to_string(height);
+		return;
+	}
+
+	int flag = settings.getBool("graphics/startfullscreen") ? SDL_WINDOW_FULLSCREEN : SDL_WINDOW_RESIZABLE;
+
+	int version = GAME_VERSION;
+	std::string windowName = "Land of Dran v" + std::to_string(version);
+
+	window = SDL_CreateWindow(windowName.c_str(),SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,width,height,SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | flag);
+	if(!window)
+	{
+		error("Failed to create window, SDL_GetError: " + std::string(SDL_GetError()));
+		return;
+	}
+
+	debug("Creating context");
+
+	context = SDL_GL_CreateContext(window);
+	if(!context)
+	{
+		error("Failed to create GL context, SDL_GetError: " + std::string(SDL_GetError()) + " glGetError: " + std:to_string(glGetError()));
+		return;
+	}
+
+	debug("Starting GLEW");
+
+	//glewExperimental = GL_TRUE; Used to need this, might not be needed anymore
+	//Must be called after window creation
+	GLenum glewError = glewInit();
+
+	if(glewError != GLEW_OK)
+	{
+		error("glewInit failed SDL_GetError: " + std::string(SDL_GetError()) + " glew error: " + std::to_string(glewError));
+		return;
+	}
+
+	if(SDL_GL_SetSwapInterval(settings.getBool("graphics/usevsync")) != 0)
+		error("SDL_GL_SetSwapInterval failed SDL_GetError: " + std::string(SDL_GetError()));
+
+	if(settings.getBool("graphics/debug"))
+		glEnable(GL_DEBUG_OUTPUT);
+
+	valid = true;
 }
 
 RenderContext::~RenderContext()
 {
-
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
 }
