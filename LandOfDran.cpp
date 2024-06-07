4a4189f4d483a768193f5ff244a3aea8ab416f9c
SorpoBlock <syerjchep@gmail.com>
2024-06-07 12:59:02 -0400

    Update LandOfDran.cpp

--- LandOfDran.cpp
+++ LandOfDran.cpp
@@ -18,19 +18,16 @@ int main(int argc, char* argv[])
 
 	info("Opening preferences file");
 
+	//Import settings we have, set defaults for settings we don't, then export the file with any new default values
 	SettingManager preferences("Config/settings.txt");
 	populateDefaults(preferences);
 	preferences.exportToFile("Config/settings.txt");
-	  
+
+	Logger::setDebug(preferences.getBool("logger/verbose"));
+
 	globalStartup(preferences);
 
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-	SDL_Window* window = SDL_CreateWindow("Land of Dran", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 1024, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-	SDL_GLContext context = SDL_GL_CreateContext(window);
-	GLenum glewError = glewInit();
+	RenderContext context(preferences);
 
 	bool doMainLoop = true;
 	while (doMainLoop)
@@ -45,7 +42,9 @@ int main(int argc, char* argv[])
 			}
 		}
 
-		SDL_GL_SwapWindow(window);
+		context.clear(1,1,1);
+		
+		context.swap();
 	}
 
 	globalShutdown();
