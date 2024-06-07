71dfb6c019134b94b746774c6a60d274c7a56ed9
SorpoBlock <syerjchep@gmail.com>
2024-06-07 10:49:59 -0400

    Add descriptions to preferences

--- Utility/DefaultPreferences.cpp
+++ Utility/DefaultPreferences.cpp
@@ -2,20 +2,24 @@
 
 void populateDefaults(SettingManager& settings)
 {
-	//Be sure to have the last parameter (overwrite) always be set to false for these
-	//Don't want to overwrite the user's intended settings
+	/*
+		Be sure to have the overwrite parameter always be set to false for these
+		Don't want to overwrite the user's intended settings
+  		Leave last parameter blank if you don't want it to show up in the options GUI
+    		Users can still edit it manually in the text file that's exported
+	*/
 
 	//Global SDL / GL settings:
-	settings.addInt("graphics/openglmajor", 3, false);
-	settings.addInt("graphics/openglminor", 3, false);
-	settings.addInt("graphics/multisamplebuffers", 1, false);
-	settings.addInt("graphics/multisamplesamples", 8, false);
-	settings.addBool("graphics/compatibilityprofile", false, false);
-	settings.addBool("graphics/debug", false, false);
+	settings.addInt("graphics/openglmajor", 	  3, false,     "");
+	settings.addInt("graphics/openglminor", 	  3, false,     "");
+	settings.addInt("graphics/multisamplebuffers", 	  1, false,     "");
+	settings.addInt("graphics/multisamplesamples", 	  8, false,     "Anti-Aliasing samples");
+	settings.addBool("graphics/compatibilityprofile", false, false, "");
+	settings.addBool("graphics/debug", 		  false, false, "OpenGL debug mode, useful if attaching RenderDoc");
 
 	//Render context specific settings:
-	settings.addInt("graphics/startresolutionx", 1024, false);
-	settings.addInt("graphics/startresolutiony", 1024, false);
-	settings.addBool("graphics/startfullscreen", false, false);
-	settings.addBool("graphics/usevsync", false, false);
+	settings.addInt("graphics/startresolutionx", 	  1024, false,  "Program X resolution to start with");
+	settings.addInt("graphics/startresolutiony", 	  1024, false,  "Program Y resolution to start with");
+	settings.addBool("graphics/startfullscreen", 	  false, false, "Start in fullscreen");
+	settings.addBool("graphics/usevsync", 		  false, false, "Limit FPS to monitor refresh speed");
 }
